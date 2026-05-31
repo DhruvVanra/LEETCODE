d826c74023b17c8d4322072a349c6123d97d29de
vanra Dhruv <vanradhruv02@gmail.com>
2026-05-31 19:01:04 +0530

    Add files via upload

---
+++ singleElementInSortedArray540.java
@@ -0,0 +1,26 @@
+class singleElementInSortedArray540{
+
+    public static int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low<high){
+           int mid = (low + high)/2;
+            if(mid%2 == 1){
+                mid--;
+            }
+
+            if(nums[mid] == nums[mid+1]){
+                low = mid+2;
+            }else{
+                high = mid;
+            }
+        }
+
+        
+        return nums[low];
+    }
+    public static void main(String[] args) {
+        int nums[] = {3,3,7,7,11,11,12};
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
