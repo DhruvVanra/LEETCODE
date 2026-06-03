844b3528d5503e217ebe8e2e76b9d4306a5a4ee6
vanra Dhruv <vanradhruv02@gmail.com>
2026-06-03 09:29:05 +0530

    Add files via upload

---
+++ timeToBuyTickets2073.java
@@ -0,0 +1,21 @@
+
+import com.sun.source.doctree.ReturnTree;
+
+class timeToBuyTIckets2073{
+    public static  int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+                count+=Math.min(tickets[i], tickets[k]);
+            }else{
+                count+= Math.min(tickets[i],tickets[k]-1);
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int tickets[] = {2,3,2};
+        System.out.println(timeRequiredToBuy(tickets, 1));
+    }
+}
