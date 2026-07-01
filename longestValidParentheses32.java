43d567e95eb7fe4a2c3b859c25bae2abb9cd3efd
vanra Dhruv <vanradhruv02@gmail.com>
2026-07-01 19:49:04 +0530

    Add files via upload

---
+++ longestValidParentheses32.java
@@ -0,0 +1,63 @@
+
+import java.util.Stack;
+
+class longestValidParentheses32{
+    public static  int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+
+        int len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+
+                st.pop();
+
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    len = Math.max(len, i - st.peek());
+                }
+            }
+        }
+
+        return len;
+    }
+
+    public static void main(String[] args) {
+       String[] testCases = {
+            "",
+            "(",
+            ")",
+            "()",
+            "()()",
+            "(())",
+            "(()",
+            "())",
+            ")()())",
+            "(()())",
+            "((()))",
+            "()(()",
+            "(()(()))",
+            "))))",
+            "((((",
+            "()(()))))",
+            "(()()",
+            "())((())",
+            "()((()))",
+            "()()()()",
+            "((()()))",
+            "(()))())(",
+            "(()(()))()",
+            "((())())",
+            "()((())())"
+        };
+
+        for (String s : testCases) {
+            System.out.println(longestValidParentheses(s));
+        }
+    }
+}
